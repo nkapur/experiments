c1b8fa4b979d57cb9022c25e6d429b7633413256
nkapur <nav17kapur@gmail.com>
2025-08-08 18:08:51 -0700

    End to end deployment Successful for fastapi-test (#17)
    * Kube manifests for FastAPI Test App
    
    * ALB Created, policy updated for new version, DNS CNAME record created
    
    * Update policy to recent version, support 3 level sub-domains for staging, recreate certificate

--- infra/personal_domain/acm.tf
+++ infra/personal_domain/acm.tf
@@ -3,7 +3,7 @@
 # The 'validation_method' is set to DNS, which is the best way to automate this.
 resource "aws_acm_certificate" "my_certificate" {
   domain_name               = aws_route53_zone.my_domain_zone.name
-  subject_alternative_names = ["*.${aws_route53_zone.my_domain_zone.name}"]
+  subject_alternative_names = ["*.${aws_route53_zone.my_domain_zone.name}", "*.staging.${aws_route53_zone.my_domain_zone.name}"]
   validation_method         = "DNS"
 
   # Recommended lifecycle block to prevent issues when replacing a certificate in use
@@ -24,6 +24,15 @@ locals {
 }
 
 # 2. Create the DNS records needed for validation
+
+# NOTE: We run into circular dependencies when updating SANs. The locals.validation_records
+#       will need to be updated to reflect the new SANs. The solution may require us to
+#       work around by getting 
+#         1) terraform to forget the resources below
+#         2) comment below
+#         3) run the above
+#         4) reset - terraform import, then uncomment below and terraform apply
+
 # This resource now uses the unique list of records from the local variable.
 resource "aws_route53_record" "acm_validation_records" {
   for_each = {
