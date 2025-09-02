6d5eb20b6b944db55d3989419dde5a4edde22838
Nav <nav17kapur@gmail.com>
2025-09-02 15:45:10 -0700

    failure fixes - change order of destroy operations; fix polling calls; for setup, verify ALB is up before declaring success

--- common/shell/bounce_app.sh
+++ common/shell/bounce_app.sh
@@ -5,15 +5,16 @@ set -e
 ACTION=${1:-up}
 APP_NAME=$2
 DEPLOYMENT_MODE=${3:-staging}
-source "${SCRIPT_DIR}/../../lib/shell/base.sh"
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 TERRAFORM_DIR="${SCRIPT_DIR}/../../app/${APP_NAME}/deploy/terraform"
 REPO_ROOT="${SCRIPT_DIR}/../.."
+source "${SCRIPT_DIR}/../../lib/shell/base.sh"
+source "${SCRIPT_DIR}/../../lib/shell/polling_utils.sh"
 
 # Verify Inputs
-if [ -z "$APP_NAME" ] || [ -z "$DIRECTION" ] || [ -z "$DEPLOYMENT_MODE" ]; then
-  echo "❌ Error: APP_NAME(=$APP_NAME), DIRECTION(=$DIRECTION), and DEPLOYMENT_MODE(=$DEPLOYMENT_MODE) must be set."
+if [ -z "$APP_NAME" ] || [ -z "$ACTION" ] || [ -z "$DEPLOYMENT_MODE" ]; then
+  echo "❌ Error: APP_NAME(=$APP_NAME), ACTION(=$ACTION), and DEPLOYMENT_MODE(=$DEPLOYMENT_MODE) must be set."
   exit 1
 fi
 
@@ -35,28 +36,47 @@ setup() {
     helm upgrade --install ${APP_NAME//_/-} \
         platform-modules/fastapi-service \
         --version 0.1.1 \
-        -f ${REPO_ROOT}/../platform_modules/app_platform/helm/environments/staging-globals.yaml \
-        -f ./deploy/helm/values-staging.yaml \
-        --namespace staging \
+        -f ${REPO_ROOT}/../platform_modules/app_platform/helm/environments/${DEPLOYMENT_MODE}-globals.yaml \
+        -f ./deploy/helm/values-${DEPLOYMENT_MODE}.yaml \
+        --namespace ${DEPLOYMENT_MODE} \
         --create-namespace
 
     # --- DNS SETUP ---
+    echo "Setting up DNS records via Terraform..."
     cd ${TERRAFORM_DIR}
     terraform init
     terraform apply -auto-approve
+
+    echo "The LB should be ready within 10-15 minutes..."
+    ALB_ARN=$(aws elbv2 describe-load-balancers \
+        --query "LoadBalancers[*].LoadBalancerArn" \
+        --output text | xargs -n1 aws elbv2 describe-tags --resource-arns \
+        | jq '.TagDescriptions[] | select(.Tags[] | select(.Key=="ingress.k8s.aws/stack" and .Value=="${DEPLOYMENT_MODE}/${APP_NAME//_/-}-fastapi-service"))' \
+        | jq -r '.ResourceArn')
+
+    ALB_READY_CHECK="aws elbv2 describe-load-balancers --load-balancer-arns ${ALB_ARN} | jq -r '.LoadBalancers[0].State.Code'"
+    poll_for_output "${ALB_READY_CHECK}" 'active' 90
+    echo "✅ ALB ARN: $ALB_ARN"
 }
 
 teardown() {
     echo "🔥 Starting safe teardown process..."
-    cd ${REPO_ROOT}/app/${APP_NAME}
-    helm uninstall ${APP_NAME//_/-} --namespace staging
 
+    # --- Destroy DNS records and other TF-managed resources ---
     cd ${TERRAFORM_DIR}
-    terraform destroy -auto-approve
+    terraform init
+    terraform destroy -target=module.fastapi_service.aws_route53_record.app_cname -auto-approve
+    echo "✅ Terraform Destroy complete"
 
+    # --- Destroy Kubernetes resources ---
+    cd ${REPO_ROOT}/app/${APP_NAME}
+    helm uninstall ${APP_NAME//_/-} --namespace ${DEPLOYMENT_MODE}
+    echo "✅ Helm Uninstall complete"
+
+    # --- Verify ingress/LB resources are terminated ---
     echo "Verifying ingress/LB resources are terminated..."
-    INGRESS=${APP_NAME//_/-}-fastapi-service
-    poll_for_output "kubectl get ingress ${INGRESS} -n ${DEPLOYMENT_MODE}" '"${INGRESS}" not found'
+    local INGRESS=${APP_NAME//_/-}-fastapi-service
+    poll_for_output "kubectl get ingress ${INGRESS} -n ${DEPLOYMENT_MODE}" "\"$INGRESS\" not found" 30 30
 
     # Terraform destroy not recommended as TF sets up the container registry for the app which we still need
     echo "✅ Teardown complete."
